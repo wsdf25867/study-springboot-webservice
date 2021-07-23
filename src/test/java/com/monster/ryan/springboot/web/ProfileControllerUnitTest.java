064683b4e8b0eebd463752d7e76d63f6941dc0dd
wsdf25867 <wsdf25867@gmail.com>
2021-07-23 15:24:53 +0900

    스프링부트와 aws로 혼자 구현하는 웹 서비스 ~383p

---
+++ src/test/java/com/monster/ryan/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,57 @@
+package com.monster.ryan.springboot.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+import org.springframework.security.core.parameters.P;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile이_조회된다(){
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real_profile이_없으면_첫_번째가_조회된다(){
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void activ_profile_없으면_default가_조회된다(){
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
