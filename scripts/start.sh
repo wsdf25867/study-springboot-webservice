e1625fce95d53fc990631b5274e37b4798fc41fd
wsdf25867 <wsdf25867@gmail.com>
2021-07-26 14:41:57 +0900

    스프링부트와 aws로 혼자 구현하는 웹 서비스 ~393p

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,20 +4,20 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITOY=/home/ec2-user/app/step3
-PROJECT_NAME=study-springboot-webservice
+REPOSITORY=/home/ec2-user/app/step3
+PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITOY/zip/*.jar $REPOSITOY/"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITOY/zip/*.jar $REPOSITOY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITOY/*.jar | tail -n 1)
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR_Name: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 권한 실행 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
@@ -26,7 +26,6 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
